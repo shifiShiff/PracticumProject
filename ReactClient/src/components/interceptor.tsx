dd4608c8e9814f2a69a5e536e077361b1a46c69c
shifiShiff <sh0527132865@gmail.com>
2025-03-22 23:35:32 +0200

    connect to server

--- ReactClient/src/components/interceptor.tsx
+++ ReactClient/src/components/interceptor.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5131/api", 
+  baseURL: process.env.REACT_APP_API_BASE_URL, 
+  // baseURL: "http://localhost:5131/api", 
   headers: {
     "Content-Type": "application/json",
   },
