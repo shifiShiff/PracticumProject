12565bedca948fed45866ed59c0f7c8de4bc8acc
shifiShiff <sh0527132865@gmail.com>
2025-03-23 01:34:33 +0200

    2

--- ReactClient/src/components/interceptor.tsx
+++ ReactClient/src/components/interceptor.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, 
+  baseURL: import.meta.env.VITE_APP_API_BASE_URL, 
   // baseURL: "http://practicumserver-5ut1.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
