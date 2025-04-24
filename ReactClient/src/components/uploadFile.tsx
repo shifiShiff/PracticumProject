cd8ee2c35a251e2808be277bcc9f9ac758a560b1
shifiShiff <sh0527132865@gmail.com>
2025-04-24 22:51:41 +0300

    AI request

--- ReactClient/src/components/uploadFile.tsx
+++ ReactClient/src/components/uploadFile.tsx
@@ -100,7 +100,7 @@ const UploadFile = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/analyze", {
+      const response = await fetch("https://practicumproject-r2h7.onrender.com/analyze", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
