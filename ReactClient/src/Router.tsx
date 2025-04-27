d85c6cb54c9627894b7f880d95c4cef30592343b
shifiShiff <sh0527132865@gmail.com>
2025-04-28 00:31:07 +0300

    component name

--- ReactClient/src/Router.tsx
+++ ReactClient/src/Router.tsx
@@ -1,11 +1,11 @@
-import AppLayout from "./components/AppLayout";
+import AppLayout from "./components/appLayout";
 import { createBrowserRouter } from "react-router-dom";
 import ImageGallery from "./components/imageGallery";
 import Register from "./components/register";
 import Login from "./components/login"; 
 import UploadFile from "./components/uploadFile";
 import Dashboard from "./components/dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "./components/protectedRoute";
 import Auth from "./components/auth";
 
 
