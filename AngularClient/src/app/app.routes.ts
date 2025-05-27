bfd714fce0b909859d2214897fbf71f3d83dcfad
shifiShiff <sh0527132865@gmail.com>
2025-05-27 16:13:24 +0300

    architecture

--- AngularClient/src/app/app.routes.ts
+++ AngularClient/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from '../components/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 
-import { DashboardComponent } from '../components/dashboard/dashboard.component';
-import { authGuard } from '../guards/auth.guard';
-import { VotegraphComponent } from '../components/votegraph/votegraph.component';
-import { ChallengeActionComponent } from '../components/challenge-action/challenge-action.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { authGuard } from './guards/auth.guard';
+import { VotegraphComponent } from './components/votegraph/votegraph.component';
+import { ChallengeActionComponent } from './components/challenge-action/challenge-action.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' }, // ברירת מחדל
--- AngularClient/src/app/services/challenge.service.ts
+++ AngularClient/src/app/services/challenge.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, tap, throwError } from 'rxjs';
-import { environment } from '../environment.prod';
+import { environment } from '../../environment.prod';
 
 @Injectable({
   providedIn: 'root'
